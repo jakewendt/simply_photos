f5fc43d6d236ebde54a39157aec5b1d2f12a1529
Jake <github@jakewendt.com>
2011-09-07 14:12:00 -0700

    Removed now duplicate tests.

--- test/functional/photos/photos_controller_test.rb
+++ test/functional/photos/photos_controller_test.rb
@@ -1,4 +1,3 @@
-#require File.dirname(__FILE__) + '/../../test_helper'
 require 'test_helper'
 
 class SimplyPhotos::PhotosControllerTest < ActionController::TestCase
@@ -20,46 +19,47 @@ class SimplyPhotos::PhotosControllerTest < ActionController::TestCase
 
 	assert_access_with_https
 	assert_access_with_login({
-		:logins => [:super_user,:admin,:editor]})
+		:logins => [:superuser,:administrator,:editor]})
+#		:logins => [:super_user,:admin,:editor]})
 
 	assert_no_access_with_http 
 	assert_no_access_with_login({ 
 		:logins => [:interviewer,:reader,:active_user] })
 	assert_no_access_without_login
 
-	assert_no_access_with_login(
-		:attributes_for_create => nil,
-		:method_for_create => nil,
-		:actions => nil,
-		:suffix => " and invalid id",
-		:login => :super_user,
-		:redirect => :photos_path,
-		:edit => { :id => 0 },
-		:update => { :id => 0 },
-		:destroy => { :id => 0 }
-	)
-
-%w( super_user admin editor ).each do |cu|
-
-	test "should NOT create invalid photo with #{cu} login" do
-		login_as send(cu)
-		assert_no_difference('Photo.count') do
-			post :create, :photo => {}
-		end
-		assert_not_nil flash[:error]
-		assert_template 'new'
-		assert_response :success
-	end
-
-	test "should NOT update invalid photo with #{cu} login" do
-		login_as send(cu)
-		put :update, :id => factory_create.id, 
-			:photo => { :title => "a" }
-		assert_not_nil flash[:error]
-		assert_template 'edit'
-		assert_response :success
-	end
-
-end
+#	assert_no_access_with_login(
+#		:attributes_for_create => nil,
+#		:method_for_create => nil,
+#		:actions => nil,
+#		:suffix => " and invalid id",
+#		:login => :super_user,
+#		:redirect => :photos_path,
+#		:edit => { :id => 0 },
+#		:update => { :id => 0 },
+#		:destroy => { :id => 0 }
+#	)
+#
+#%w( super_user admin editor ).each do |cu|
+#
+#	test "should NOT create invalid photo with #{cu} login" do
+#		login_as send(cu)
+#		assert_no_difference('Photo.count') do
+#			post :create, :photo => {}
+#		end
+#		assert_not_nil flash[:error]
+#		assert_template 'new'
+#		assert_response :success
+#	end
+#
+#	test "should NOT update invalid photo with #{cu} login" do
+#		login_as send(cu)
+#		put :update, :id => factory_create.id, 
+#			:photo => { :title => "a" }
+#		assert_not_nil flash[:error]
+#		assert_template 'edit'
+#		assert_response :success
+#	end
+#
+#end
 
 end
